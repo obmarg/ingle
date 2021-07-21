f06f3105c4967ef2a76fdf472c7345a3fa5c4be5
Graeme Coupar <grambo@grambo.me.uk>
2021-07-21 21:20:42 +0100

    Add generated GRPC code

---
+++ ingle/src/google/mod.rs
@@ -0,0 +1,16 @@
+#[path = "google.api.rs"]
+pub mod api {}
+
+pub mod firestore {
+    #[path = "google.firestore.v1.rs"]
+    pub mod v1 {}
+}
+
+#[path = "google.protobuf.rs"]
+pub mod protobuf {}
+
+#[path = "google.r#type.rs"]
+pub mod r#type {}
+
+#[path = "google.rpc.rs"]
+pub mod rpc {}
