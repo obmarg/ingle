42f6e950739dbf584c902ca374005eefa71a84bd
Graeme Coupar <grambo@grambo.me.uk>
2021-07-23 22:10:05 +0100

    Some stuff I missed

--- ingle/src/requests.rs
+++ ingle/src/requests.rs
@@ -1,24 +1,5 @@
 use crate::{path::CollectionPath, values::DocumentValues};
 
-pub struct AddDocumentRequest {
-    collection_path: CollectionPath,
-    document_id: String,
-    document: DocumentValues,
-}
-
-impl AddDocumentRequest {
-    pub(crate) fn new(
-        collection_path: CollectionPath,
-        document_id: String,
-        document: DocumentValues,
-    ) -> Self {
-        Self {
-            collection_path,
-            document_id,
-            document,
-        }
-    }
-}
 
 pub struct DocumentResponse<D> {
     pub name: String,
